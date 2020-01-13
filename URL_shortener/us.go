5a08f32867a6ed31d7844aba5d6116c7f56cab34
lehieulol <42810931+lehieulol@users.noreply.github.com>
2020-01-13 23:56:46 +0700

    add edit save

--- URL_shortener/us.go
+++ URL_shortener/us.go
@@ -3,22 +3,34 @@ package main
 import(
 	"net/http"
 	"log"
+	"io/ioutil"
+	"html/template"
 )
 
-var m = map[string]string{
-	"go": "https://golang.org",
-	"gci": "https://codein.withgoogle.com",
-}
-
 func redirect(w http.ResponseWriter, r *http.Request) {
-	source := r.URL.Path[1:];
-	if source != "" {
-		http.Redirect(w, r, m[source], http.StatusFound)
+	source := r.URL.Path[len("/goto/"):];
+	dest, err := ioutil.ReadFile("URL\\"+source+".txt");
+	if(err != nil){
+		http.Redirect(w, r, "/edit/"+source, http.StatusFound);
+		return
+	}else{
+		http.Redirect(w, r, string(dest), http.StatusFound);
 	}
 }
-
+func add(w http.ResponseWriter, r *http.Request){
+	source := r.URL.Path[len("/edit/"):];
+	t, _ := template.ParseFiles("edit.html");
+	t.Execute(w, source)
+}
+func save(w http.ResponseWriter, r *http.Request){
+	source := r.URL.Path[len("/save/"):];
+	a := r.FormValue("body");
+	ioutil.WriteFile("URL\\"+source+".txt",[]byte(a),0600);
+	http.Redirect(w,r,"/goto/"+source,http.StatusFound);
+}
 func main() {
-    http.HandleFunc("/", redirect)
-    
+	http.HandleFunc("/edit/", add);
+	http.HandleFunc("/goto/", redirect);
+	http.HandleFunc("/save/", save);
     log.Fatal(http.ListenAndServe("", nil));
 }
